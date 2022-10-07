2e3c692fe566d9ace9ff3d56002c1cb3d30bfe0c
DevUmair22 <devumair22@gmail.com>
2022-10-07 14:55:54 -0700

    Complete CRUD for ecommerce

--- index.js
+++ index.js
@@ -2,107 +2,25 @@ const express = require('express');
 const app =express();
 const mongoose = require('mongoose');
 require('dotenv/config');
-const Post= require('./models/User');
-const port =8080;
+// const Post= require('./models/User');
+const productRoute =require('./routes/product');
+const cartRoute =require ('./routes/cart');
+const orderRoute =require('./routes/order')
 const userRoute =require('./routes/users');
 const authRoute = require('./routes/auth')
+const port =8080;
 
-
-// Its a middle ware
+// Middle ware Functions to use
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-// app.set('veiw engine', 'ejs');
-
 app.use('/user', userRoute);
 app.use('/user/auth' ,authRoute);
-
-//Routes
-app.get('/', async (req , res)=>
-{
-    // res.render('index');
-    // res.sendFile(path.join(__dirname+ '/index.ejs'));
-    // res.send(`Hello , This is my first api working over port ${port}`);
- try {
-    const allPosts = await Post.find();
-    res.json(allPosts);
-    console.log(allpostsb)
- }catch(err){
-    res.json({message: err});
- }
-});
-
-app.get('/:22', async(req,res)=>
-{
-    //findOne() for any type of data
-    //findById() will return data with the given id
-    //find() to project all data
-    // find({},{projection:{param1:0=>means no value of param1, param2:1means all param2 values}})
-    try{
-        const specific= await Post.find({},{projection:{firstName:0}});
-        console.log(specific);
-        res.json(specific);
-    }catch(err)
-    {
-        res.json({message:err});
-    }
-})
-
-app.delete('/:id', async (req,res)=>
-{
-    try{
-        const removedPost= await Post.deleteMany({firstName:req.params.id});
-        res.json(removedPost);
-    }catch(err)
-    {
-        res.json({message:err});
-    }
-});
-
-app.post('/' , async(req, res)=>
-{   
-    console.log('req.body', req.body);
-     try{
-const post= await new Post({
-    iD: req.body.iD,
-firstName:req.body.firstName,
-secondName:req.body.secondName,
-email:req.body.email,
-phoneNumber:req.body.phoneNumber
-});
-
-post.save()
-.then(data=>{
-    res.json(data);
-})
-}catch(err){
-    res.json({message: err})
-}
-// res.send(`Your username is ${req.body.firstName} ${req.body.secondName} && password is ${req.body.password} && phone number is ${req.body.phoneNo}`)
-// console.log(req.body);
-});
+app.use('/products', productRoute)
+app.use('/cart', cartRoute);
+app.use('/orders', orderRoute)
 
 
-app.put('/:phone', async (req,res)=>
-{
-
-try{
-
-    const update = await Post.updateMany({phoneNumber:req.params.phone},{$set:{phoneNumber:req.body.phoneNumber}});
-    console.log(update)
-    res.json(update);
-}catch(err)
-{
-    res.json({message:err});
-}
-//    console.log(req.params.iD);
-// const update =await Post.find(c=>c.phoneNumber===parseInt(req.params.phone))
-// (!update)? res.status(404).send('The data with the given id was not found'):  
-// console.log(update);
-// const firstName = req.body.firstName;
-// res.send(update);
-
-});
 //DB Connection
 mongoose.connect(process.env.DB_CONNECTION, ()=>{
     console.log('Connected to DataBase!')
@@ -114,3 +32,91 @@ mongoose.connect(process.env.DB_CONNECTION, ()=>{
 app.listen( port ,()=>{
     console.log(`Server started at http://localhost:${port}`)
 });
+
+
+//Routes
+// app.get('/', async (req , res)=>
+// {
+//     // res.render('index');
+//     // res.sendFile(path.join(__dirname+ '/index.ejs'));
+//     // res.send(`Hello , This is my first api working over port ${port}`);
+//  try {
+//     const allPosts = await Post.find();
+//     res.json(allPosts);
+//     console.log(allpostsb)
+//  }catch(err){
+//     res.json({message: err});
+//  }
+// });
+
+// app.get('/:22', async(req,res)=>
+// {
+//     //findOne() for any type of data
+//     //findById() will return data with the given id
+//     //find() to project all data
+//     // find({},{projection:{param1:0=>means no value of param1, param2:1means all param2 values}})
+//     try{
+//         const specific= await Post.find({},{projection:{firstName:0}});
+//         console.log(specific);
+//         res.json(specific);
+//     }catch(err)
+//     {
+//         res.json({message:err});
+//     }
+// })
+
+// app.delete('/:id', async (req,res)=>
+// {
+//     try{
+//         const removedPost= await Post.deleteMany({firstName:req.params.id});
+//         res.json(removedPost);
+//     }catch(err)
+//     {
+//         res.json({message:err});
+//     }
+// });
+
+// app.post('/' , async(req, res)=>
+// {   
+//     console.log('req.body', req.body);
+//      try{
+// const post= await new Post({
+//     iD: req.body.iD,
+// firstName:req.body.firstName,
+// secondName:req.body.secondName,
+// email:req.body.email,
+// phoneNumber:req.body.phoneNumber
+// });
+
+// post.save()
+// .then(data=>{
+//     res.json(data);
+// })
+// }catch(err){
+//     res.json({message: err})
+// }
+// // res.send(`Your username is ${req.body.firstName} ${req.body.secondName} && password is ${req.body.password} && phone number is ${req.body.phoneNo}`)
+// // console.log(req.body);
+// });
+
+
+// app.put('/:phone', async (req,res)=>
+// {
+
+// try{
+
+//     const update = await Post.updateMany({phoneNumber:req.params.phone},{$set:{phoneNumber:req.body.phoneNumber}});
+//     console.log(update)
+//     res.json(update);
+// }catch(err)
+// {
+//     res.json({message:err});
+// }
+// //    console.log(req.params.iD);
+// // const update =await Post.find(c=>c.phoneNumber===parseInt(req.params.phone))
+// // (!update)? res.status(404).send('The data with the given id was not found'):  
+// // console.log(update);
+// // const firstName = req.body.firstName;
+// // res.send(update);
+
+// });
--- routes/cart.js
+++ routes/cart.js
@@ -1,7 +1,69 @@
 const router = require("express").Router();
-const Post= require('../models/User');
+const Cart = require("../models/Cart");
+const {  verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
+
+//Create New Cart
+router.post('/',  verifyTokenAndAuthorization, async (req,res)=>{
+const newCart = new Cart(req.body)
+try{
+    const savedCart = await newCart.save();
+res.status(200).json(savedCart);
+console.log("Cart saved successfully")
+}catch(err){
+    res.status(500).json(err);
+    console.log("Cart saving unsuccessful")
+}
+});
+
+//Update a Cart
+router.put('/update/:id',  verifyTokenAndAuthorization, async(req,res)=>
+{
+
+try {
+   const updatedCart= await Cart.findByIdAndUpdate(req.params.id,
+   {$set:req.body},
+   {new:true}
+   );
+   res.status(200).json(updatedCart);
+}catch(err){
+   res.status(500).json(err);
+}
 
 
 
+});
 
-module.exports = router;
+//Delete Cart by ID
+router.delete('/delete/:id',  verifyTokenAndAuthorization, async (req, res)=>{
+   try{
+await Cart.findByIdAndDelete(req.params.id)
+res.status(200).json("Cart has been deleted Successfully")
+   }catch(err){
+res.status(500).json(err);
+   }
+});
+
+//Get User Cart
+router.get('/find/:userId',  verifyTokenAndAuthorization, async (req, res)=>{                      
+   try{
+const cart =await Cart.findOne({userId: req.params.userId});
+res.status(200).json(cart);                           //status code 200 means ok 
+   }catch(err){
+res.status(500).json(err);                             //status code 500 means internal server error
+   }
+});
+
+
+//Get All Carts
+router.get('/', verifyTokenAndAdmin,  async (req, res)=>{
+try{
+
+
+    const carts = await Cart.find();
+    res.status(200).json(carts);
+}catch(err){
+    res.status(500).json(err);
+}
+});
+
+module.exports =router;
