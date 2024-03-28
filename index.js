9a57aadc38896d989eb4d6b2a1e4f2edf0be1ea8
DevUmair22 <108020563+DevUmair22@users.noreply.github.com>
2024-03-28 16:22:52 -0400

    minor change

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
 const express = require('express');
-const app =express();
+const app = express();
 const mongoose = require('mongoose');
 require('dotenv/config');
 // const Post= require('./models/User');
-const productRoute =require('./routes/product');
-const cartRoute =require ('./routes/cart');
-const orderRoute =require('./routes/order')
-const userRoute =require('./routes/users');
+const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order')
+const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth')
-const port =8080;
+const port = 8080;
+var cors = require('cors')
 
 // Middle ware Functions to use
-
+app.use(cors())
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use('/user', userRoute);
-app.use('/user/auth' ,authRoute);
+app.use('/user/auth', authRoute);
 app.use('/products', productRoute)
 app.use('/cart', cartRoute);
 app.use('/orders', orderRoute)
 
 
 //DB Connection
-mongoose.connect(process.env.DB_CONNECTION, ()=>{
+mongoose.connect(process.env.DB_CONNECTION, () => {
     console.log('Connected to DataBase!')
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err);
 })
 
 //To listen on the port 
-app.listen( port ,()=>{
+app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`)
 });
 
--- routes/product.js
+++ routes/product.js
@@ -1,82 +1,81 @@
 const router = require("express").Router();
 const Product = require("../models/Product");
-const {  verifyTokenAndAdmin } = require("./verifyToken");
+const { verifyTokenAndAdmin } = require("./verifyToken");
 
 //Create New Product
-router.post('/add', verifyTokenAndAdmin, async (req,res)=>{
-const newProduct = new Product(req.body)
-try{
-    const savedProduct = await newProduct.save();
-res.status(200).json(savedProduct);
-console.log("Product saved successfully")
-}catch(err){
-    res.status(500).json(err);
-    console.log("Product saving unsuccessful")
-}
+router.post('/add', verifyTokenAndAdmin, async (req, res) => {
+   const newProduct = new Product(req.body)
+   try {
+      const savedProduct = await newProduct.save();
+      res.status(200).json(savedProduct);
+      console.log("Product saved successfully")
+   } catch (err) {
+      res.status(500).json(err);
+      console.log("Product saving unsuccessful")
+   }
 });
 
 //Update a Product by ID
-router.put('/update/:id', verifyTokenAndAdmin, async(req,res)=>
-{
+router.put('/update/:id', verifyTokenAndAdmin, async (req, res) => {
 
-try {
-   const updatedProduct= await Product.findByIdAndUpdate(req.params.id,
-   {$set:req.body},
-   {new:true}
-   );
-   res.status(200).json(updatedProduct);
-}catch(err){
-   res.status(500).json(err);
-}
+   try {
+      const updatedProduct = await Product.findByIdAndUpdate(req.params.id,
+         { $set: req.body },
+         { new: true }
+      );
+      res.status(200).json(updatedProduct);
+   } catch (err) {
+      res.status(500).json(err);
+   }
 
 
 
 });
 
 //Delete Product by ID
-router.delete('/delete/:id', verifyTokenAndAdmin, async (req, res)=>{
-   try{
-await Product.findByIdAndDelete(req.params.id)
-res.status(200).json("Product has been deleted Successfully")
-   }catch(err){
-res.status(500).json(err);
+router.delete('/delete/:id', verifyTokenAndAdmin, async (req, res) => {
+   try {
+      await Product.findByIdAndDelete(req.params.id)
+      res.status(200).json("Product has been deleted Successfully")
+   } catch (err) {
+      res.status(500).json(err);
    }
 });
 
-//Get Products
-router.get('/find/:id',  async (req, res)=>{                      
-   try{
-const product =await Product.findById(req.params.id);
-res.status(200).json(product);                           //status code 200 means ok 
-   }catch(err){
-res.status(500).json(err);                             //status code 500 means internal server error
+//Get Product
+router.get('/find/:id', async (req, res) => {
+   try {
+      const product = await Product.findById(req.params.id);
+      res.status(200).json(product);                           //status code 200 means ok 
+   } catch (err) {
+      res.status(500).json(err);                             //status code 500 means internal server error
    }
 });
 
 
 //Get All Products
-router.get('/find',  async (req, res)=>{
-    const qNew =req.query.new;                          //if you write a ?new=true  in params it will store it in query
-    const qCategory =req.params.category;
-    try{
-let products;
-if(qNew){
-    products =await Product.find().sort({createdAt: -1}).limit(5);
+router.get('/', async (req, res) => {
+   const qNew = req.query.new;                          //if you write a ?new=true  in params it will store it in query
+   const qCategory = req.params.category;
+   try {
+      let products;
+      if (qNew) {
+         products = await Product.find().sort({ createdAt: -1 }).limit(5);
 
-}else if (qCategory){
-    products = await Product.find({
-        categories:{$in: [qCategory],},
-    });
-}else {
-    products = await Product.find();
-}
-
-
- res.status(200).json(products)                     //status code 200 means ok 
-    }catch(err){
- res.status(500).json(err);                             //status code 500 means internal server error
-    }
- });
+      } else if (qCategory) {
+         products = await Product.find({
+            categories: { $in: [qCategory], },
+         });
+      } else {
+         products = await Product.find();
+      }
 
 
-module.exports =router;
+      res.status(200).json(products)                     //status code 200 means ok 
+   } catch (err) {
+      res.status(500).json(err);                             //status code 500 means internal server error
+   }
+});
+
+
+module.exports = router;
